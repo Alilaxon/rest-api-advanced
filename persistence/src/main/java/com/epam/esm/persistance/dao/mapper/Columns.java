c3b1a96c5e508d654135ad1ef7d727bb5c441fa8
Alilaxon <firkraagin@gmail.com>
2023-05-30 23:03:45 +0300

    first update

---
+++ persistence/src/main/java/com/epam/esm/persistance/dao/mapper/Columns.java
@@ -0,0 +1,24 @@
+package com.epam.esm.persistance.dao.mapper;
+
+public class Columns {
+
+    public static final String ID = "id";
+
+    public static final String GIFT_ID = "gift_id";
+
+    public static final String GIFT_NAME = "gift_name";
+
+    public static final String DESCRIPTION = "description";
+
+    public static final String PRICE = "price";
+
+    public static final String DURATION = "duration";
+
+    public static final String CREATE_DATE = "create_date";
+
+    public static final String lAST_UPDATE_DATE = "last_update_date";
+
+    public static final String TAG_ID = "tag_id";
+
+    public static final String TAG_NAME = "tag_name";
+}
