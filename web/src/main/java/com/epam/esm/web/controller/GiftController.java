c0f58617aec757e32f0acfb7f61a20331d50d338
Alilaxon <firkraagin@gmail.com>
2023-07-13 02:20:29 +0300

    added class UrlsPart and Update GiftController

--- web/src/main/java/com/epam/esm/web/controller/GiftController.java
+++ web/src/main/java/com/epam/esm/web/controller/GiftController.java
@@ -1,11 +1,13 @@
 package com.epam.esm.web.controller;
+
 import com.epam.esm.model.dto.GiftDTO;
 import com.epam.esm.model.exception.*;
-import com.epam.esm.persistance.entity.GiftCertificate;
 import com.epam.esm.model.service.GiftService;
 import com.epam.esm.model.service.impl.GiftServiceImpl;
+import com.epam.esm.persistance.entity.GiftCertificate;
 import com.epam.esm.web.utils.GiftLinker;
 import com.epam.esm.web.utils.Sorter;
+import com.epam.esm.web.utils.UrlParts;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +22,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-@RequestMapping( "/gift")
+@RequestMapping(UrlParts.GIFTS)
 public class GiftController {
 
     private static final Logger log = LogManager.getLogger(GiftController.class);
@@ -33,36 +35,36 @@ public class GiftController {
         this.giftCertificateService = giftCertificateService;
     }
 
-    @GetMapping("/get-all")
+    @GetMapping()
     public List<GiftDTO> getAll() {
         log.info("get_all");
         return GiftLinker.addLinkToGiftDTO(giftCertificateService.getAll());
 
     }
 
-    @GetMapping("/getAllByTag")
+    @GetMapping("/byTag")
     public List<GiftCertificate> getAllByTag
-            (@RequestParam("tag") String tag ,
+            (@RequestParam("tag") String tag,
              @RequestParam("order") String order,
-            @RequestParam(name = "page",required = false, defaultValue = "1")Long page,
-             @RequestParam(name = "size",required = false, defaultValue = "50")Long size)
+             @RequestParam(name = "page", required = false, defaultValue = "1") Long page,
+             @RequestParam(name = "size", required = false, defaultValue = "50") Long size)
             throws NoSuchTagNameException {
 
-        log.info("get all by tag = {}",tag);
-        if (order.equals("desc")){
+        log.info("get all by tag = {}", tag);
+        if (order.equals("desc")) {
             log.info("sort all by desc");
-           return Sorter.sorting(giftCertificateService.getAllByTag(tag,page,size));
+            return Sorter.sorting(giftCertificateService.getAllByTag(tag, page, size));
         }
         log.info("sort all by asc");
-        return giftCertificateService.getAllByTag(tag,page,size);
+        return giftCertificateService.getAllByTag(tag, page, size);
     }
 
-    @GetMapping("/getAllByDescription")
-    public List<GiftCertificate> getAllByDescription (
-            @RequestParam("description")String description,
-            @RequestParam("order") String order){
-        log.info("get all by description = {}",description);
-        if (order.equals("desc")){
+    @GetMapping("/byDescription")
+    public List<GiftCertificate> getAllByDescription(
+            @RequestParam("description") String description,
+            @RequestParam("order") String order) {
+        log.info("get all by description = {}", description);
+        if (order.equals("desc")) {
             log.info("sort all by desc");
             return Sorter.sorting(giftCertificateService.getAllByDescription(description));
         }
@@ -71,18 +73,19 @@ public class GiftController {
     }
 
 
-    @PostMapping("/create")
+    @PostMapping(UrlParts.CREATE)
     public ResponseEntity<Long> create(@RequestBody GiftDTO giftDto)
             throws GiftNameIsReservedException, InvalidTagException, InvalidGiftDtoException {
-        log.info("Gift '{}' will be create",giftDto.getName());
+        log.info("Gift '{}' will be create", giftDto.getName());
         Long id = giftCertificateService.create(giftDto).getId();
 
         return ResponseEntity.status(HttpStatus.CREATED).body(id);
     }
-    @GetMapping("/read/{id}")
+
+    @GetMapping(UrlParts.READ + UrlParts.ID)
     @ResponseStatus(HttpStatus.OK)
-    public GiftDTO getById (@PathVariable("id") Long id) throws NoSuchGiftException {
-        log.info("Get Gift by id = '{}'",id);
+    public GiftDTO getById(@PathVariable("id") Long id) throws NoSuchGiftException {
+        log.info("Get Gift by id = '{}'", id);
         GiftDTO giftDTO = giftCertificateService.get(id);
         Link selfLink = linkTo(methodOn(GiftController.class).getById(
                 giftDTO.getId())).withSelfRel();
@@ -90,22 +93,20 @@ public class GiftController {
         return giftDTO.add(selfLink);
     }
 
-    @PatchMapping("/update/{id}")
-    public ResponseEntity<Long> create (@PathVariable("id") Long id,
-                                        @RequestBody GiftDTO giftDto){
-        log.info("Update Gift by id = '{}'",id);
-        Long resultId = giftCertificateService.update(id,giftDto).getId();
+    @PatchMapping(UrlParts.UPDATE + UrlParts.ID)
+    public ResponseEntity<Long> create(@PathVariable("id") Long id,
+                                       @RequestBody GiftDTO giftDto) {
+        log.info("Update Gift by id = '{}'", id);
+        Long resultId = giftCertificateService.update(id, giftDto).getId();
 
         return ResponseEntity.status(HttpStatus.CREATED).body(resultId);
     }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Object> delete (@PathVariable Long id){
-        log.info("Delete Gift by id = '{}'",id);
+    @DeleteMapping(UrlParts.DELETE + UrlParts.ID)
+    public ResponseEntity<Object> delete(@PathVariable Long id) {
+        log.info("Delete Gift by id = '{}'", id);
         giftCertificateService.deleteById(id);
 
         return ResponseEntity.noContent().build();
     }
-
-
 }
---
+++ web/src/main/java/com/epam/esm/web/utils/UrlParts.java
@@ -0,0 +1,22 @@
+package com.epam.esm.web.utils;
+
+public class UrlParts {
+
+    public static final String GIFTS = "/gifts";
+
+    public static final String TAGS = "/tags";
+
+    public static final String USERS = "/users";
+
+    public static final String ORDERS = "/orders";
+
+    public static final String CREATE = "/create";
+
+    public static final String READ = "/read";
+
+    public static final String UPDATE = "/update";
+
+    public static final String DELETE = "/delete";
+
+    public static final String ID = "/{id}";
+}
